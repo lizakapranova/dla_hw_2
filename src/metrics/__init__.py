862b2a01e0dc4a21727cec4bdb961db26701fdcc
BeeMaxim <msbekoev@edu.hse.ru>
2024-11-22 22:25:40 +0300

    add SDRi

--- src/metrics/__init__.py
+++ src/metrics/__init__.py
@@ -1,4 +1,5 @@
 from src.metrics.example import ExampleMetric
 from src.metrics.si_snri import SiSNRi
+from src.metrics.sdri import SDRi
 from src.metrics.stoi import STOI
 from src.metrics.pesq import PESQ
